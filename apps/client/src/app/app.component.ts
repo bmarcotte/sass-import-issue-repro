0bfd96ec75b57cd5cd4b8e477e3335bcda497822
carniatto <mateus.carniatto@realimapctanalytics.com>
2023-02-26 11:17:14 +0100

    Initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular15-sass-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
